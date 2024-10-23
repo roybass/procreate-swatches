e168f5aeb92381fe06c98f0dab2d713e141a537b
unknown <roybass@gmail.com>
2024-10-23 14:50:42 +0300

    configure port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const JSZip = require('jszip');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 function hexToHsvEntry(hex) {
